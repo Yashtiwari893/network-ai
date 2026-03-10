05b69d2ea5636319bf95a38d5cd1821616b5eecc
Yash Tiwari <121632237+Yashtiwari893@users.noreply.github.com>
2026-03-10 14:24:14 +0530

    add connection check logic

--- utilities/connetion.js
+++ utilities/connetion.js
@@ -4,7 +4,12 @@ const options = {
   socketTimeoutMS: 45000,
 };
 
+if (!process.env.MONGODB_URL) {
+  console.error("FATAL ERROR: MONGODB_URL environment variable is not defined!");
+}
+
 const mongoDB = mongoose.createConnection(process.env.MONGODB_URL, options);
+
 
 mongoDB.on('connected', () => console.log('MongoDB connected through createConnection'));
 mongoDB.on('error', (err) => console.error('MongoDB error through createConnection:', err));
